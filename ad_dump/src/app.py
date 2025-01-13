3d2ddb4515d7b23e9efe98e1aa07cfceb483fb14
Dan <51533129+the2dl@users.noreply.github.com>
2025-01-13 08:33:43 -0500

    switch backend port

--- ad_dump/src/app.py
+++ ad_dump/src/app.py
@@ -420,4 +420,4 @@ def get_group_members(group_id):
     return {"error": "Failed to fetch group members"}, 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, host='0.0.0.0')
+    app.run(debug=True, port=4501, host='0.0.0.0')
